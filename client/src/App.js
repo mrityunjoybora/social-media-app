bac6a455361c5516f689091abb430e5bf129272a
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-01-20 22:51:58 +0530

    Add: Routes

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="//profile/:username" element={<Profile />} />
+
+        {/* <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
