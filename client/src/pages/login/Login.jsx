5630c72497a6599f5e486573993b69b3134d337b
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-01-30 15:40:59 +0530

    Fix: Login form

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,15 @@
+import { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+
+  const email = useRef();
+  const password = useRef();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+  }
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +20,15 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form onSubmit={handleClick} className="loginBox">
+            <input placeholder="Email" type="email" ref={email} required className="loginInput" />
+            <input placeholder="Password" ref={password} required type="password" minLength="6" className="loginInput" />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
