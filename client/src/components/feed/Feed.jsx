a066fe955fb467a9b9998f5f5e4a7aec81052eab
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-02-22 14:53:37 +0530

    Fix: share component on hompage

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -14,9 +14,8 @@ export default function Feed({ username}) {
   const { user } = useContext(AuthContext);
   
   useEffect(() => {
-  
     const fetchPosts = async () => {
-      const res = username ? await axios.get("/posts/profile/" + user.username) : await axios.get("/posts/timeline/" + user._id);
+      const res = username ? await axios.get("/posts/profile/" + username) : await axios.get("/posts/timeline/" + user._id);
       setPost(
         res.data.sort((p1,p2) => { 
           return new Date(p2.createdAt) - new Date(p1.createdAt);
@@ -24,13 +23,13 @@ export default function Feed({ username}) {
       );
     };
     fetchPosts();
-  }, [user.username, user._id]);
+  }, [username, user._id]);
   
   
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
+        {(!username || username===user.username) &&<Share />}
         {post.map((p) => (
           <Post key={p._id} post={p} />
         ))}
